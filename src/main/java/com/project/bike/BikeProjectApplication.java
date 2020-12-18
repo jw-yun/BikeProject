1c3da639a123f4e3a5dd1c569e8dc439d805d51f
jw-yun <yjw42>
2020-12-18 11:06:24 +0900

    Initial commit

---
+++ src/main/java/com/project/bike/BikeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bike/BikeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
