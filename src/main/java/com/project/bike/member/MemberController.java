ab23a95dbd67d765c6f41c560b8c5e4cc73b3bd8
jw-yun <yjw42>
2020-12-20 22:56:34 +0900

    q

--- src/main/java/com/project/bike/member/MemberController.java
+++ src/main/java/com/project/bike/member/MemberController.java
@@ -1,5 +1,41 @@
 package com.project.bike.member;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping("/member/**")
 public class MemberController {
 
+		@Autowired
+		private MemberService memberService;
+	
+		@PostMapping("memberLogin")
+		public ModelAndView getMemberLogin(MemberVO memberVO, HttpSession session) throws Exception{
+			ModelAndView mv = new ModelAndView();
+			memberVO = memberService.getMemberLogin(memberVO);
+			
+			if(memberVO!=null) {
+				session.setAttribute("member", memberVO);
+				mv.setViewName("redirect:../");
+			}
+			else {
+				mv.addObject("msg", "로그인 실패");
+				mv.addObject("path", "./memberLogin");
+				mv.setViewName("common/result");
+			}
+			
+			return mv;
+		}
+		@GetMapping("memberLogout")
+		public String getMemberLogout(HttpSession session) throws Exception{
+			session.invalidate();
+			return "redirect:../";
+		}
+		
 }
--- src/main/webapp/WEB-INF/views/member/memberLogin.jsp
+++ src/main/webapp/WEB-INF/views/member/memberLogin.jsp
@@ -1,12 +1,33 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<div class="container">
+	<div>
+		<img alt="" src="../resources/images/logo.png">
+	</div>
+	<form action="./memberLogin" method="post">
+		<div>
+			<table>
+				<tr>
+					<td>
+						<input type="text" name="id" placeholder="아이디">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="password" name="pw" placeholder="비밀번호" required="required">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="checkbox" value="로그인 상태 유지">
+					</td>
+				</tr>
 
-</body>
-</html>
+				
+			</table>
+	
+	
+		</div>
+	
+	</form>
+</div> 
